2178319c1b32b728b3b42ddb74c5544b70f84191
atomicpages <djtthompson@gmail.com>
2019-12-04 12:49:43 -0800

    1.0.1
    * Ensuring assertion failures do not exit normally
    * Reexporting @pika/plugin-build-node manifest and beforeJob functions
    * Including src/ in main tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import builtinModules from 'builtin-modules';
 import json from '@rollup/plugin-json';
 import rollupBabel from 'rollup-plugin-babel';
 
+import { manifest as baseManifest, beforeJob as baseBeforeJob } from '@pika/plugin-build-node';
+
 import { BuilderOptions as PikaBuilderOptions } from '@pika/types';
 import { rollup } from 'rollup';
 
@@ -45,6 +47,9 @@ function processPlugins(plugins?: Plugins) {
         return m;
     });
 }
+
+export const manifest = baseManifest;
+export const beforeJob = baseBeforeJob;
 
 export async function build({ out, reporter, options = {} }: BuilderOptions): Promise<void> {
     const writeToNode = path.join(out, 'dist-node', 'index.js');
--- test/assert.js
+++ test/assert.js
@@ -1,33 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
 
 const pkg = path.join(__dirname, 'pkg');
 const folders = fs.readdirSync(pkg);
 
-console.assert(fs.existsSync(pkg), 'pkg directory expected to exist');
+const assert = (assertion, ...messages) => {
+    if (!assertion) {
+        console.error(messages);
+        throw new Error(messages);
+    }
+};
 
-console.assert(
-    Array.prototype.includes.apply(folders, [
-        'dist-node',
-        'dist-src',
-        'dist-types',
-        'package.json',
-    ]),
-    `Expected to see:\n${JSON.stringify(
-        ['dist-node', 'dist-src', 'dist-types', 'package.json'],
-        null,
-        4
-    )}\n\nbut saw:\n\n${JSON.stringify(folders, null, 4)}`
-);
+try {
+    assert(fs.existsSync(pkg), 'pkg directory expected to exist');
 
-console.assert(
-    fs.readFileSync(path.join(__dirname, 'pkg/dist-node/index.js')).toString() ===
-        `'use strict';
+    assert(
+        Array.prototype.includes.apply(folders, [
+            'ddist-node',
+            'dist-src',
+            'dist-types',
+            'package.json',
+        ]),
+        `Expected to see:\n${JSON.stringify(
+            ['dist-node', 'dist-src', 'dist-types', 'package.json'],
+            null,
+            4
+        )}\n\nbut saw:\n\n${JSON.stringify(folders, null, 4)}`
+    );
+
+    assert(
+        fs.readFileSync(path.join(__dirname, 'pkg/dist-node/index.js')).toString() ===
+            `'use strict';
 
 var foo=true;var foo$1 = {foo:foo};
 
 console.log(foo$1);
 //# sourceMappingURL=index.js.map
 `,
-    'FIles do not match'
-);
+        'FIles do not match'
+    );
+} catch (e) {
+    console.error(e.messages);
+    process.exit(1);
+}
